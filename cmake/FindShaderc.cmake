d45a9e4466d300886b82b98087467bdd3961d906
Marijn Suijten <m.suijten@students.uu.nl>
2019-10-13 23:21:04 +0200

    CMake: Shaderc: Expose simple linkable imported target.

--- cmake/FindShaderc.cmake
+++ cmake/FindShaderc.cmake
@@ -3,8 +3,14 @@
 #
 # This will will define the following variables:
 #
-# SHADERC_FOUND    - true if Shaderc has been found
-# SHADERC_GLSLC    - the glslc executable
+# Shaderc_FOUND    - true if Shaderc has been found
+# Shaderc_GLSLC    - the glslc executable
+# Shaderc::shaderc - linkable target
+
+if (TARGET Shaderc::shaderc)
+	# Already found
+	return()
+endif()
 
 find_program(Shaderc_GLSLC glslc)
 
@@ -17,15 +23,22 @@ set(shaderc_LIB_SEARCH_DIRS
 	"/usr/local/lib"
 	"${CMAKE_SOURCE_DIR}/lib")
 
-find_path(SHADERC_INCLUDE_DIR "shaderc/shaderc.hpp"
+find_path(Shaderc_INCLUDE_DIR "shaderc/shaderc.hpp"
 	PATHS ${shaderc_HEADER_SEARCH_DIRS})
-find_library(SHADERC_LIBRARY
-	NAMES shaderc_shared shaderc_combined
+find_library(Shaderc_LIBRARY
+	NAMES shaderc_shared
 	PATHS ${shaderc_LIB_SEARCH_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Shaderc
 	DEFAULT_MSG
 	Shaderc_GLSLC
-	SHADERC_LIBRARY
-	SHADERC_INCLUDE_DIR)
+	Shaderc_LIBRARY
+	Shaderc_INCLUDE_DIR)
+
+add_library(Shaderc::shaderc SHARED IMPORTED GLOBAL)
+target_include_directories(Shaderc::shaderc INTERFACE ${Shaderc_INCLUDE_DIR})
+
+set_target_properties(Shaderc::shaderc
+	PROPERTIES
+	IMPORTED_LOCATION "${Shaderc_LIBRARY}")
